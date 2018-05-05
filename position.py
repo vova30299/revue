c5c4719a0953ed505666878c06ec326376d46f91
vova30299 <37497790+vova30299@users.noreply.github.com>
2018-05-06 01:02:34 +0300

    Update position.py
    Функции, обрабатывающие состояние каждой ячейки океана

--- position.py
+++ position.py
@@ -1,1 +1,40 @@
+def neighborhood(this, N, S, W, E, NW, NE, SW, SE):  # функция, подсчитывающая число соседей
+    neighbors = 0  # этого же вида
+    if (N == this):
+        neighbors += 1
+    if (S == this):
+        neighbors += 1
+    if (W == this):
+        neighbors += 1
+    if (E == this):
+        neighbors += 1
+    if (NW == this):
+        neighbors += 1
+    if (NE == this):
+        neighbors += 1
+    if (SW == this):
+        neighbors += 1
+    if (SE == this):
+        neighbors += 1
+    return (neighbors)
 
+
+def generation(animal, N, S, W, E, NW, NE, SW, SE):  # функция, проверяющая, должно ли
+    count = 0  # появиться существо в пустой клетке
+    if (N == animal):
+        count += 1
+    if (S == animal):
+        count += 1
+    if (W == animal):
+        count += 1
+    if (E == animal):
+        count += 1
+    if (NW == animal):
+        count += 1
+    if (NE == animal):
+        count += 1
+    if (SW == animal):
+        count += 1
+    if (SE == animal):
+        count += 1
+    return (count)
