6ea0f09bf580fdb96a225952fd322bab72d56dfd
vova30299 <37497790+vova30299@users.noreply.github.com>
2018-05-06 01:03:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import argparse
 import state_of_the_ocean
+import position
 
 nothing = 0  # ничего
 rock = 1  # скала
@@ -18,48 +19,6 @@ select = 0
 
 parser = argparse.ArgumentParser(description = 'Process some integers.')
 parser.add_argument('-m', '--model', type = str)
-
-def neighborhood(this, N, S, W, E, NW, NE, SW, SE):  # функция, подсчитывающая число соседей
-    neighbors = 0  # этого же вида
-    if (N == this):
-        neighbors += 1
-    if (S == this):
-        neighbors += 1
-    if (W == this):
-        neighbors += 1
-    if (E == this):
-        neighbors += 1
-    if (NW == this):
-        neighbors += 1
-    if (NE == this):
-        neighbors += 1
-    if (SW == this):
-        neighbors += 1
-    if (SE == this):
-        neighbors += 1
-    return (neighbors)
-
-
-def generation(animal, N, S, W, E, NW, NE, SW, SE):  # функция, проверяющая, должно ли
-    count = 0  # появиться существо в пустой клетке
-    if (N == animal):
-        count += 1
-    if (S == animal):
-        count += 1
-    if (W == animal):
-        count += 1
-    if (E == animal):
-        count += 1
-    if (NW == animal):
-        count += 1
-    if (NE == animal):
-        count += 1
-    if (SW == animal):
-        count += 1
-    if (SE == animal):
-        count += 1
-    return (count)
-
 
 while (select != 3):
     print("МЕНЮ:")
